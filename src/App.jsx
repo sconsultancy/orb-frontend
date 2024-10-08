98ba95c3d9131b699a435ae4273e9c2758b7fab0
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-10-08 16:50:09 +0530

    loginScreen

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import { Outlet } from "react-router-dom";
+import { Toaster, toast } from "sonner";
 
 function App() {
   return (
     <div>
       <h1>
+        <Toaster position="top-center" richColors />
         <Header></Header>
         <Outlet></Outlet>
       </h1>
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import { toast } from "sonner";
 
 function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/");
+    }
+  }, [userInfo, navigate]);
 
   const handleFocusInput = (e) => {
     const labels = document.getElementsByTagName("label");
@@ -47,8 +65,16 @@ function LoginScreen() {
 
   const handleSubmitClick = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+
+    try {
+      const res = await login({ email, password }).unwrap();
+      dispatch(setCredentials({ ...res }));
+      navigate("/");
+    } catch (err) {
+      toast.error(err?.data?.message || err.message);
+    }
   };
+
   const handleGoogleSignIn = async (e) => {
     e.preventDefault();
 
